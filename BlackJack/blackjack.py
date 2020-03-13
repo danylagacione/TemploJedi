f5344d2f52ff35c40a412003e263f57926bc97f7
danylagacione <danielycrys24@gmail.com>
2020-03-13 11:57:06 -0300

    developing BlackJack-att

--- BlackJack/blackjack.py
+++ BlackJack/blackjack.py
@@ -1,44 +1,42 @@
 
-# As cartas utilizadas e valores serão conforme abaixo:
-# "2" => 2
-# "3" => 3
-# "4" => 4
-# "5" => 5
-# "6" => 6
-# "7" => 7
-# "8" => 8
-# "9" => 9
-# "10" => 10
-# "A" => 1
-# "J" => 10
-# "Q" => 10
-# "K" => 10
-#
-# O jogo deverá embaralhar as cartas acima. O jogo deve pedir para o jogador virar uma cartão,
-# quando ele virar, deverá apresentar o score de acordo com a carta.
-#
-# Obs.: O "A" terá o valor 1 quando tiver outro número já virado.
-#
-# Segue exemplo abaixo:
-# ["A"] ==> 11
-# ["A", "J"] ==> 21
-# ["A", "10", "A"] ==> 12
-# ["5", "3", "7"] ==> 15
-# ["5", "4", "3", "2", "A", "K"] ==> 25
-# jogador
-#croupier
-#valete, dama e reis = 10 pontos
-# ás 1 ou 10 pontos de acordo com a vontade do jogador
-
 import random
 
-class BlackJack:
-    def __init__(self):
-        self.lista_cartas = ['A','1', '2', '3', '4', '5','6', '7', '8','9', '10',{'K':10, 'J':10, 'Q':10}]
-        self.play = True
+#cartas  = [ '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A']
+valores  = [{'carta 2': 2}, {'carta 3': 3}, {'carta 4': 4}, {'carta 5': 5},{'carta 6': 6}, {'carta 7': 7}, {'carta 8': 9},
+            {'carta 9': 9}, {'carta 10': 10}, {'carta J': 10}, {'carta Q': 10}, {'carta K': 10}, {'carta A': 11}]
+quantidade = 1
+valor = 0
+contador = 0
 
-    def embaralhar_cartas(self, lista_cartas):
-        pass
+while (quantidade > 0):
+    virar_carta = int(input('Deseja virar carta? (1-Yes / 0-No):'))
+    if virar_carta == 1:
+        escolha = random.choice(valores)
+        print(escolha)
+        continue
+    if virar_carta == 0:
+        break
 
-    def mesa(self):
-        pass
+    for i in valores:
+        if i ==  :
+            if i == 'A':
+                if valor == 0:
+                    valor = valor + 11
+                else:
+                    valor = valor + 1
+            else:
+                valor = valor + valores[i]
+            print(f"Valor: {valor}")
+    contador += 1
+
+
+#
+#         # else:
+#     #         #     quantia = quantidade - 1
+#     #         #     print(f'Score:{valor}')
+#
+# def pegar_carta(baralho):
+#     escolhido = random.choice(baralho)
+#     indice = baralho.index(escolhido)
+#     baralho[indice]
+#     return escolhido
